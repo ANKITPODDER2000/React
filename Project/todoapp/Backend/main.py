06299a6902bb4999f15e8f96895f12aa843f5f8e
Ankit Podder <ankitpodder0211@gmail.com>
2021-10-15 13:46:56 +0530

    add todo done

--- Project/todoapp/Backend/main.py
+++ Project/todoapp/Backend/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+import uuid
+
+class Todo(BaseModel):
+	Key : Optional[str] = str(uuid.uuid4())
+	title : str
+	givenBy : str
+	givenDate : Optional[str] = str(datetime.now().strftime('%Y-%m-%d'))
+	submittedBy : str
+	content : str
+
 todos = [
 	{
 		'Key': 1,
 		'title': 'Do math problems...',
 		'givenBy': 'Math Teacher',
-		'givenDate': datetime.now(),
-		'submittedBy': datetime.now(),
+		'givenDate': str(datetime.now().strftime('%Y-%m-%d')),
+		'submittedBy': str(datetime.now().strftime('%Y-%m-%d')),
 		'content':
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolorem adipisci autem expedita neque laudantium! Aliquid at veritatis dolorum vel quod earum saepe reprehenderit sit exercitationem, suscipit sequi delectus dolor eveniet ratione doloremque, alias corporis? Nobis odit necessitatibus officiis non. Necessitatibus quibusdam nobis corporis neque placeat optio inventore magnam id, aliquam ad minus quidem omnis, consectetur consequuntur, porro quas reiciendis.'
 	},
@@ -15,8 +27,8 @@ todos = [
 		'Key': 2,
 		'title': 'Do js problems...',
 		'givenBy': 'JavaScript Teacher',
-		'givenDate': datetime.now(),
-		'submittedBy': datetime.now(),
+		'givenDate': str(datetime.now().strftime('%Y-%m-%d')),
+		'submittedBy': str(datetime.now().strftime('%Y-%m-%d')),
 		'content':
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente esse iure voluptates sed itaque asperiores repudiandae provident, sequi eum aspernatur labore iste porro vel consequatur nemo error dolore nostrum ad necessitatibus sunt cupiditate veritatis! Quas voluptatibus veniam ratione blanditiis enim harum? Veniam, corrupti adipisci. Aperiam, nostrum adipisci tempora quae, et corrupti cum distinctio vero facere dolorum cumque error ab ipsum id nam? Nesciunt, ullam sit! Sapiente animi ex pariatur facere nihil quidem! Culpa blanditiis dolor, explicabo tempore dolores facilis recusandae corrupti optio in error perferendis ex iure delectus saepe magnam unde, iste cupiditate quo ad porro quas commodi aperiam natus?'
 	},
@@ -24,8 +36,8 @@ todos = [
 		'Key': 3,
 		'title': 'Do DSA problems...',
 		'givenBy': 'Data Structure Teacher',
-		'givenDate': datetime.now(),
-		'submittedBy': datetime.now(),
+		'givenDate': str(datetime.now().strftime('%Y-%m-%d')),
+		'submittedBy': str(datetime.now().strftime('%Y-%m-%d')),
 		'content':
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum asperiores consectetur dolor. Facilis voluptatibus, id consequatur, excepturi dicta tempore iusto omnis nostrum ducimus aspernatur facere quidem dignissimos? Accusantium, modi sit.'
 	}
@@ -48,3 +60,9 @@ def home():
 @app.get("/todo")
 def get_alltodo():
     return todos
+
+@app.post("/todo/add-todo")
+def add_todo(todo : Todo):
+	todo.Key = str(uuid.uuid4())
+	todos.append(todo)
+	return todo
--- Project/todoapp/src/AddTodo.js
+++ Project/todoapp/src/AddTodo.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import './CSS/AddTodo.css';
 
@@ -10,37 +11,60 @@ class AddTodo extends Component {
 			givenBy: '',
 			submittedBy: ''
 		};
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-
+	handleSubmit(e) {
+		let content = {
+			title: this.state.title,
+			givenBy: this.state.givenBy,
+			content: this.state.description,
+			submittedBy: this.state.submittedBy
+		};
+		axios
+			.post('http://127.0.0.1:8000/todo/add-todo', content)
+			.then((x) => alert('Todo is added successfully ...'))
+			.catch((err) => console.log(err));
+		this.setState({
+			title: '',
+			description: '',
+			givenBy: '',
+			submittedBy: ''
+		});
+		e.preventDefault();
+	}
 	render() {
 		return (
 			<div className="form-container">
-				<div className="form-inner-container">
+				<form className="form-inner-container" onSubmit={this.handleSubmit}>
 					<h1>Add Todo</h1>
 					<input
 						type="text"
 						placeholder="Todo Title"
 						value={this.state.title}
 						onChange={(e) => this.setState({ title: e.target.value })}
+						required
 					/>
 					<input
 						type="text"
 						placeholder="Given By"
 						value={this.state.givenBy}
 						onChange={(e) => this.setState({ givenBy: e.target.value })}
+						required
 					/>
 					<textarea
 						placeholder="Description"
 						value={this.state.description}
 						onChange={(e) => this.setState({ description: e.target.value })}
+						required
 					/>
 					<input
 						type="date"
 						value={this.state.submittedBy}
 						onChange={(e) => this.setState({ submittedBy: e.target.value })}
+						required
 					/>
 					<input type="submit" placeholder="Submit" className="btn" />
-				</div>
+				</form>
 			</div>
 		);
 	}
